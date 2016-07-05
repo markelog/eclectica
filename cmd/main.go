9b3d533cdc1e0755aeb2211896d6eced72d1237c
Oleg Gaidarenko <markelog@gmail.com>
2016-07-05 18:47:00 +0300

    Extract archive

--- cmd/main.go
+++ cmd/main.go
@@ -5,41 +5,48 @@ import (
   "fmt"
   "time"
 
+  "github.com/markelog/eclectica/detect"
+  "github.com/markelog/archive"
+
   "github.com/urfave/cli"
   "github.com/cavaliercoder/grab"
-  "github.com/markelog/eclectica/detect"
 )
 
-func download(url string) {
+func download(url string) string {
 
-  // start file download
+  // Start file download
   fmt.Printf("Downloading %s...\n", url)
-  respch, err := grab.GetAsync(".", url)
+
+  respch, err := grab.GetAsync(os.TempDir(), url)
+
   if err != nil {
     fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", url, err)
     os.Exit(1)
   }
 
-  // block until HTTP/1.1 GET response is received
+  // Block until HTTP/1.1 GET response is received
   fmt.Printf("Initializing download...\n")
+
   resp := <-respch
 
-  // print progress until transfer is complete
+  // Print progress until transfer is complete
   for !resp.IsComplete() {
     fmt.Printf("\033[1AProgress %d / %d bytes (%d%%)\033[K\n", resp.BytesTransferred(), resp.Size, int(100*resp.Progress()))
     time.Sleep(200 * time.Millisecond)
   }
 
-  // clear progress line
+  // Clear progress line
   fmt.Printf("\033[1A\033[K")
 
-  // check for errors
+  // Check for errors
   if resp.Error != nil {
     fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", url, resp.Error)
     os.Exit(1)
   }
 
-  fmt.Printf("Successfully downloaded to ./%s\n", resp.Filename)
+  fmt.Printf("Downloaded\n")
+
+  return resp.Filename
 }
 
 
@@ -57,6 +64,8 @@ Usage: e <name>, <name>@<version>
       fmt.Println(err)
     }
 
-    download(dists["url"])
+    path := download(dists["url"])
+
+    archive.Extract(path, os.TempDir())
   }
 }
