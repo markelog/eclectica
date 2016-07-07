e902b1cc645e4fdd7160377dbbc352c514cd7be9
Oleg Gaidarenko <markelog@gmail.com>
2016-07-07 23:01:00 +0300

    Correct length of `Directories` list

--- variables/variables.go
+++ variables/variables.go
@@ -7,7 +7,7 @@ import(
 
 var (
   Home = fmt.Sprintf("%s/.eclectica/versions", os.Getenv("HOME"))
-  Directories = [1]string{"bin", "include", "share", "lib"}
+  Directories = [4]string{"bin", "include", "share", "lib"}
   Prefix = "/usr/local"
 )
 
