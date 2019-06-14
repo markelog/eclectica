3bb5316ad7982f8fa2402f281ad31b3a38153309
Oleg Gaidarenko <markelog@gmail.com>
2019-06-14 12:16:45 +0100

    Bump version of default yarn installation

--- plugins/nodejs/yarn.go
+++ plugins/nodejs/yarn.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	minimalForYarn, _ = semver.Make("6.0.0")
-	version           = "1.13.0"
+	version           = "1.16.0"
 	yarnURL           = fmt.Sprintf(
 		"https://yarnpkg.com/downloads/%s/yarn-v%s.tar.gz",
 		version,
